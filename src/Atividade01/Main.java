566d35109976554b6d73d213e40632613622d98d
isgomesss <isadoragogomes@gmail.com>
2026-07-31 19:28:19 -0300

    Primeira aulda de Java - Faculdade

---
+++ src/Atividade01/Main.java
@@ -0,0 +1,9 @@
+package Atividade01;
+
+public class Main { //essa é nossa classe "principal" e publica.
+    public static void main(String[] args) { //Aqui é onde ficara o "corpo" do nosso codigo
+        // "publico, estatico, vazio, main, entre parentes String, que é o tipo primitivo, e agrs de argumentos
+
+        System.out.println("Hello World");
+    }
+}
